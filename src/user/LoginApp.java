f724e7e17b2545b4a7b00d2e0f397a2984049b1b
RaHaTex261 <you@example.com>
2025-03-24 20:16:55 +0400

    MAJ 3.0

--- src/user/LoginApp.java
+++ src/user/LoginApp.java
@@ -3,10 +3,9 @@ package user;
 public class LoginApp {
 
     public static void main(String[] args) {
-        
-        LoginUser LoginFrame = new LoginUser();
-        LoginFrame.setVisible(true);
-        LoginFrame.pack();
-        LoginFrame.setLocationRelativeTo(null);
+        LoginUser loginFrame = new LoginUser(); 
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
     }
-    
+}
