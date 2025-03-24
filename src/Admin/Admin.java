3750b26d5f57aa0e8cca12fa9a036ba2d24bf8fb
A11 <adavidraf1149@gmail.com>
2025-03-24 21:31:43 +0300

    Ajouter class Admin.java pour lancer LoginAdmin

---
+++ src/Admin/Admin.java
@@ -0,0 +1,13 @@
+package Admin;
+
+public class Admin {
+
+	public static void main(String[] args) {
+		LoginAdmin admin = new LoginAdmin();
+		admin.setVisible(true);
+		admin.pack();
+		admin.setLocationRelativeTo(null);
+
+	}
+
+}
